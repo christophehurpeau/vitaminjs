0a43a77aec3c7b56c40738a4d0268017d24fea5e
Lucas <lucas@evaneos.com>
2017-11-17 18:50:42 +0100

    attempt to rewrite babelrc creator function

--- packages/vitaminjs-build/config/build/babelrc.js
+++ packages/vitaminjs-build/config/build/babelrc.js
@@ -1,17 +1,122 @@
-import presetEnv from 'babel-preset-env';
-import presetReact from 'babel-preset-react';
-import presetStage1 from 'babel-preset-stage-1';
-import pluginReactRequire from 'babel-plugin-react-require';
-import pluginTransformExportDefaultName from 'babel-plugin-transform-export-default-name-forked';
-import pluginMinifyReplace from 'babel-plugin-minify-replace';
-import pluginNodeEnvInline from 'babel-plugin-transform-node-env-inline';
-import pluginMinifyDeadCodeElimination from 'babel-plugin-minify-dead-code-elimination';
-import pluginMinifyGuardedExpressions from 'babel-plugin-minify-guarded-expressions';
-import pluginDiscardModuleReferences from 'babel-plugin-discard-module-references';
-import pluginReactJsxSource from 'babel-plugin-transform-react-jsx-source';
-import pluginReactJsxSelf from 'babel-plugin-transform-react-jsx-self';
-import { vitaminResolve } from '../utils';
+const presetEnv = require('babel-preset-env');
+const presetReact = require('babel-preset-react');
+const presetStage1 = require('babel-preset-stage-1');
+const pluginReactRequire = require('babel-plugin-react-require');
+const pluginTransformExportDefaultName = require('babel-plugin-transform-export-default-name-forked');
+const pluginMinifyReplace = require('babel-plugin-minify-replace');
+const pluginNodeEnvInline = require('babel-plugin-transform-node-env-inline');
+const pluginMinifyDeadCodeElimination = require('babel-plugin-minify-dead-code-elimination');
+const pluginMinifyGuardedExpressions = require('babel-plugin-minify-guarded-expressions');
+const pluginDiscardModuleReferences = require('babel-plugin-discard-module-references');
+const pluginReactJsxSource = require('babel-plugin-transform-react-jsx-source');
+const pluginReactJsxSelf = require('babel-plugin-transform-react-jsx-self');
+const { vitaminResolve } = require('../utils');
 
+// order is: first to last
+const plugins = [
+    // Make optional the explicit import of React in JSX files
+    {
+        plugin: pluginReactRequire,
+    },
+    // Adds component stack to warning messages
+    {
+        plugin: pluginReactJsxSource,
+        dev: true,
+    },
+    // Adds __self attribute to JSX which React will use for some warnings
+    {
+        plugin: pluginReactJsxSelf,
+        dev: true,
+    },
+    // replace process.env.NODE_ENV by its current value
+    {
+        plugin: pluginNodeEnvInline,
+    },
+    // replace IS_CLIENT and IS_SERVER
+    {
+        plugin: pluginMinifyReplace,
+        options: { replacements: envSpecificReplacements('client') },
+        env: 'client',
+    },
+    {
+        plugin: pluginMinifyReplace,
+        options: { replacements: envSpecificReplacements('server') },
+        env: 'server',
+    },
+    // Dead code elimination (for example: if (IS_CLIENT) { ... } becames if (false) { }
+    {
+        plugin: pluginMinifyDeadCodeElimination,
+        options: { keepFnName: true },
+        jest: false,
+    },
+    // transforms `IS_CLIENT && doSomething()` => `false && doSomething()` to `false`
+    {
+        plugin: pluginMinifyGuardedExpressions,
+        jest: false,
+    },
+    // Remove server-only or client-only imports
+    {
+        plugin: pluginDiscardModuleReferences,
+        jest: false,
+    },
+    // easier debugging on export default arrow functions with the filename
+    {
+        plugin: pluginTransformExportDefaultName,
+        jest: false,
+    },
+];
+
+function envSpecificReplacements(env) {
+    return [
+        {
+            identifierName: 'IS_CLIENT',
+            replacement: { type: 'booleanLiteral', value: env === 'client' },
+        },
+        {
+            identifierName: 'IS_SERVER',
+            replacement: { type: 'booleanLiteral', value: env === 'server' },
+        },
+    ];
+}
+
+module.exports = (env, options) => ({
+    // order is: last to first
+    presets: [
+        [presetEnv, {
+            modules: false,
+            useBuiltIns: true,
+            targets: env !== 'client' ? { node: 'current' }
+                : { browsers: options.client.targetBrowsers },
+        }],
+        presetReact,
+        presetStage1,
+    ].filter(Boolean),
+    plugins: plugins
+        .filter(pluginsPredicate(env, options))
+        .map(({ plugin, options = {} }) => [plugin, options]),
+    sourceRoot: vitaminResolve(),
+});
+
+function pluginsPredicate(env, options) {
+    return function predicate(plugin) {
+        return (
+            // Should exclude if env is restricted and not the same
+            (!('env' in plugin) || plugin.env === env) &&
+            shouldInclude(plugin, options, 'dev') &&
+            shouldInclude(plugin, options, 'jest')
+        );
+    };
+}
+
+function shouldInclude(plugin, options, key) {
+    if (!(key in plugin)) return true;
+    return (
+        (plugin[key] && options[key]) ||
+        (!plugin[key] && !options[key])
+    );
+}
+
+/*
 export default (env, options) => ({
     // order is: last to first
     presets: [
@@ -58,3 +163,4 @@ export default (env, options) => ({
     ].filter(Boolean),
     sourceRoot: vitaminResolve(),
 });
+*/
